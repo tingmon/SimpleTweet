1deb8bc9f112f762e78bf7c4f1bf9d28778fa6a6
tingmon <Tjeong2320@conestogac.on.ca>
2022-07-05 16:01:32 -0400

    text change

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,59 +1,79 @@
 import { authService } from "fbase";
 import react, { useState } from "react";
 
-const AuthForm =() => {
-    const [error, setError] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(true);
+const AuthForm = () => {
+	const [error, setError] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [newAccount, setNewAccount] = useState(true);
 
-    const onChange = (event) =>{
-        console.log(event.target.name);
-        // console.log(event.target.value);
-        const {target: {name, value}} = event;
-        console.log(value);
-        if(name === "email"){
-            setEmail(value)
-        }
-        else if(name === "password"){
-            setPassword(value);
-        }
-    }
-    
-    const onSubmit = async (event) =>{
-        event.preventDefault();
-        try{
-            let data;
-            if(newAccount){
-                // create account
-                data = await authService.createUserWithEmailAndPassword(
-                    email, password
-                );
-            }
-            else{
-                // login
-                data = await authService.signInWithEmailAndPassword(
-                    email, password
-                );
-            }
-            console.log(data);
-        }
-        catch(error){
-            setError(error.message);
-        }
-    }
-    const toggleAccount = () => setNewAccount((prev) => !prev);
-    return(
-    <>
-        <form onSubmit={onSubmit} className="container">
-            <input className="authInput" onChange={onChange} name="email" type="email" placeholder="Email" required value={email}></input>
-            <input className="authInput" onChange={onChange} name="password" type="password" placeholder="Password" required value={password}></input>
-            <input className="authInput authSubmit" type="submit" value={newAccount ? "Create Account" : "Sign In"}></input>
-            {error && <span className="authError">{error}</span>}
-        </form>
-        <span className="authSwitch" onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
-    </>
-    )
-}
+	const onChange = (event) => {
+		console.log(event.target.name);
+		// console.log(event.target.value);
+		const {
+			target: { name, value },
+		} = event;
+		console.log(value);
+		if (name === "email") {
+			setEmail(value);
+		} else if (name === "password") {
+			setPassword(value);
+		}
+	};
+
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			let data;
+			if (newAccount) {
+				// create account
+				data = await authService.createUserWithEmailAndPassword(
+					email,
+					password
+				);
+			} else {
+				// login
+				data = await authService.signInWithEmailAndPassword(email, password);
+			}
+			console.log(data);
+		} catch (error) {
+			setError(error.message);
+		}
+	};
+	const toggleAccount = () => setNewAccount((prev) => !prev);
+	return (
+		<>
+			<form onSubmit={onSubmit} className="container">
+				<input
+					className="authInput"
+					onChange={onChange}
+					name="email"
+					type="email"
+					placeholder="Email"
+					required
+					value={email}
+				></input>
+				<input
+					className="authInput"
+					onChange={onChange}
+					name="password"
+					type="password"
+					placeholder="Password"
+					required
+					value={password}
+				></input>
+				<input
+					className="authInput authSubmit"
+					type="submit"
+					value={newAccount ? "Create Account" : "Sign In"}
+				></input>
+				{error && <span className="authError">{error}</span>}
+			</form>
+			<span className="authSwitch" onClick={toggleAccount}>
+				{newAccount ? "I Have Account" : "I Don't Have Account"}
+			</span>
+		</>
+	);
+};
 
 export default AuthForm;
